c30e63a4ee75dda91e944db414102da0975acda4
Ernestobolkin <ernest1047@gmail.com>
2022-01-16 22:59:50 +0200

    🐛

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,8 @@
 import { AddUser } from "./components/11.1_express_with-react/addUser";
-import { BrowserRouter, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
-          <AddUser />
-      </BrowserRouter>
+      <AddUser />
     </>
   );
 }
